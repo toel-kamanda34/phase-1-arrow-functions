b5c8eaebb7a6336d739292442b2f4955a122fa15
tkamanda <toelokemwa@gmail.com>
2024-07-21 14:44:00 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,10 @@
+//function expression called divide
+const divide =  function(a,b) {
+    return a/b;
+};
+
+//arrow function called square
+const square = (x) => x * x;
+
+//arrow function called add
+const add = (a,b) => a + b;
